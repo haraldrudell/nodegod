a2eb71d9162c4d3c225f6c45203febfd6f80c514
Harald Rudell <harald@therudells.com>
2012-07-02 20:00:01 -0700

    enhanced websockets communication and updates, new date format

--- public/javascripts/frontgod.js
+++ public/javascripts/frontgod.js
@@ -1,9 +1,127 @@
 // frontgod.js
 
 if (typeof NODEGOD == 'undefined') NODEGOD = {}
-
+if (typeof io == 'undefined') alert ('socket.io is missing')
 if (typeof $ == 'undefined') alert ('jQuery is missing')
-else $(function() {
+
+// on parsing
+// socket client with in-page status update
+;
+(function () {
+
+	// on page loaded
+	$(function() {
+
+		// click on any of the action buttons
+		$('.control').click(function() {
+
+			// find action word
+			var action = this.className.match(/\bdo([^ $]+)/)
+			var app
+			if (action) {
+				var app = $(this).closest('div').attr('id')
+				if (app) app = app.substring(3)
+			} else {
+				action = this.className.match(/\b(reload)($|\s)/)
+				if (action) app = true
+			}
+
+			if (action && app) socketSend({ type: action[1], app: app})
+		})
+
+		// click on reconnect, a button enabled when we are not connected
+		$('.reconnect').click(function() {
+			reconnect()
+		})
+
+		updateState() // state when page is loaded
+
+	})
+
+	function messageFromServer(data) {
+		var string
+
+		if (!data || !data.type) {
+			var text = JSON.stringify(data)
+			alert('corrupt comms:' + text)
+		} else {
+			switch(data.type) {
+			case 'denied':
+				string = 'Read-Only Mode'
+				break
+			case 'auth':
+				string = data.value === true ? 'Logged in' : data.value
+				break
+			case 'apps':
+				string = 'Got apps:' + data.apps
+				break
+			case 'app':
+				string = 'Update to ' + data.app.name
+				updateCreateApp(data.app)
+				break
+			case 'ok':
+				string = 'ok'
+				break
+			default:
+				string = 'Bad type from server:' + JSON.stringify(data.type)
+			}
+			$('#status').text(string + ' at ' + dateString(Date.now()))
+		}
+	}
+
+	function updateCreateApp(app) {
+		var div = $('#app' + app.id)
+		if (!div) $('#status').text('Unknown app:' + app.id)
+		else {
+			// get enable pattern for buttons
+			var state = app.state || 'stop'
+			var map = actionMap[state]
+			if (map) {
+				div.find('.control').each(function(index, el) {
+					var buttonAction = el.className.match(/\bdo([^ $]+)/)
+					if (buttonAction) {
+						buttonAction = buttonAction[0]
+						if (map[buttonAction]) $(el).show('slow')
+						else $(el).hide('slow')
+					}
+				})
+			}
+			div.find('.pid').text(app.pid)
+			div.find('.state').text(app.state)
+			div.find('.watchers').text(app.watchers)
+			div.find('.upsince').text(dateString(app.lastLaunch))
+			div.find('.lastcrash').text(dateString(app.lastCrash))
+			div.find('.crashes').text(app.crashCount || 0)
+			div.find('.exit').text(app.exitCode != null ? app.exitCode : '?')
+		}
+	}
+
+	var timezoneMinutesOffUtc = -new Date().getTimezoneOffset()
+	function dateString(value) {
+		var result = '?'
+		if (typeof value == 'number') {
+			var str = new Date(value + timezoneMinutesOffUtc * 60000).toISOString()
+			var hms = str.substring(11, 19)
+			var date = str.substring(0, 10)
+			if (timezoneMinutesOffUtc) {
+				var absoluteValue = Math.abs(timezoneMinutesOffUtc)
+				hms += (timezoneMinutesOffUtc < 0 ? '-' : '+') + getTwoDigits(absoluteValue / 60)
+				var minutes = absoluteValue % 60
+				if (minutes) hms += getTwoDigits(minutes)
+			} else {
+				hms += 'Z'
+			}
+			var result = hms + ' on ' + date
+		}
+		return result
+	}
+	function getTwoDigits(number) {
+		var result = number.toString()
+		if (result.length < 2) {
+				result = '0' + result
+		}
+		return result
+	}
 	var actions = [ 'dodebug', 'donodebug', 'dostart', 'dorestart', 'dostop']
 	var actionMap = {
 		'run': {
@@ -29,143 +147,54 @@ else $(function() {
 		},
 	}
 
-	$('.control').click(function() {
-
-		// find action word
-		var action = this.className.match(/\bdo([^ $]+)/)
-		var app
-		if (action) {
-			var app = $(this).closest('div').attr('id')
-			if (app) app = app.substring(3)
-		} else {
-			action = this.className.match(/\b(reload)($|\s)/)
-			if (action) app = true
-		}
-
-		if (action && app) messageToServer({ type: action[1], app: app})
-	})
-
-	connect()
-
-	function messageToServer(data) {
-		if (!connected) connect(afterConnect)
-		else afterConnect()
-
-		function afterConnect() {
-			$('#status').text('Sending to server')
-			socketSend(data)
-		}
-	}
-
-	function connect(cb) {
-		$('#status').text('Connecting to server')
-		socketConnect(messageFromServer, noConnection)
-
-		function noConnection(err) {
-			if (err) {
-				var s = 'No connection'
-				if (err) s+= ': ' + err.message
-				$('#status').text(s)
-			} else {
-				if (cb) cb()
-				else {
-					$('#status').text('Connected')
-				}
-			}
-		}
-
-		function messageFromServer(data) {
-			var string
-
-			if (!data || !data.type) {
-				var text = JSON.stringify(data)
-				alert('corrupt comms:' + text)
-			} else {
-				switch(data.type) {
-				case 'denied':
-					string = 'Read-Only Mode'
-					break
-				case 'auth':
-					string = data.value === true ? 'Logged in' : data.value
-					break
-				case 'apps':
-					string = 'Got apps:' + data.apps
-					break
-				case 'app':
-					updateCreateApp(data.app)
-					break
-				case 'ok':
-					string = 'ok'
-					break
-				default:
-					string = 'Bad type from server:' + JSON.stringify(data.type)
-				}
-				$('#status').text(string)
-			}
-		}
-	}
-
-	function updateCreateApp(app) {
-		console.log('updating:' + app.id)
-		var div = $('#app' + app.id)
-		if (!div) $('#status').text('Unknown app:' + app.id)
-		else {
-			// get enable pattern for buttons
-			var state = app.state || 'stop'
-			var map = actionMap[state]
-			if (map) {
-				div.find('.control').each(function(index, el) {
-					var buttonAction = el.className.match(/\bdo([^ $]+)/)
-					if (buttonAction) {
-						buttonAction = buttonAction[0]
-						if (map[buttonAction]) $(el).show('slow')
-						else $(el).hide('slow')
-					}
-				})
-			}
-			div.find('.pid').text(app.pid)
-			div.find('.id').text(app.id)
-			div.find('.state').text(app.state)
-			div.find('.watchers').text(app.watchers)
-			div.find('.upsince').text(app.lastLaunch ? new Date(app.lastLaunch) : '?')
-			div.find('.lastcrash').text(app.lastCrash ? new Date(app.lastCrash): '?')
-			div.find('.crashes').text(app.crashCount || 0)
-			div.find('.exit').text(app.exitCode != null ? app.exitCode : '?')
-		}
-	}
-
 	NODEGOD.socketSend = socketSend
-	var socket
-	var connected
-
-	// ensure connected, then callback
-	function socketConnect(rxFunc, callback) {
-
-		// if we are already connected, return
-		if (connected) callback()
-
-		// make sure socket.io javascript is loaded
-		else if (typeof io == 'undefined') {
-			var e = 'socket io not loaded'
-			alert(e)
-			callback(Error(e))
-		} else {
-
-			// connect the socket
-			socket = io.connect()
-			socket.on('connect_failed', function () {
-				callback(Error('connect_failed'))
-			})
-			socket.on('connect', function () {
-				connected = true
-				callback()
-			})
-			socket.on('fromgod', rxFunc)
-		}
-	}
 
 	function socketSend(data) {
 		socket.emit('togod', data)
 	}
 
-})
+	var texts = {
+		0: 'Disconnected',
+		1: 'Connecting',
+		2: 'Connected',
+		3: 'Rejected',
+	}
+	var socket = io.connect()
+	socket.on('connect', function () {
+		updateState(2)
+	})
+	socket.on('connect_failed', function (reason) {
+		updateState(3)
+	})
+	socket.on('fromgod', function (data) {
+		messageFromServer(data)
+	})
+	socket.on('disconnect', function (reason) {
+		updateState(1)
+	})
+	socket.on('error', function (error) {
+		console.log('websocket error:' + error)
+	})
+	function getState(newState) {
+		var result = socket.aState || 0
+		if (typeof newState == 'number' && newState >= 0) socket.aState = newState
+		return result
+	}
+	function reconnect() {
+		socket.socket.reconnect()
+	}
+	function updateState(newState) {
+		var lastState = getState(newState)
+		if (lastState != newState) {
+			if (typeof newState != 'number') newState = lastState
+			reconnectEnable(newState != 2)
+			//$('.connected').text()
+			$('#status').text(texts[newState] + ' at ' + dateString(Date.now()))
+		}
+	}
+	function reconnectEnable(flag) {
+		var e = $('.reconnect')
+		if (flag) e.removeAttr('disabled')
+		else e.attr('disabled', 'disabled')
+	}
+})()
--- routes/godview.js
+++ routes/godview.js
@@ -28,10 +28,64 @@ function index(request, res) {
 		apps: apps,
 		launch: launchTime,
 		readWrite: readWrite,
+		dateString: dateString,
 	})
 }
 
 function setTitle(title) {
 	appName = title
 	launchTime  = Date.now()
+}
+// pacific time adjusted for daylight savings
+var timezoneMinutesOffUtc = isDs(new Date()) ? -420 : -480
+function dateString(value) {
+	var result = '?'
+	if (typeof value == 'number') {
+		var str = new Date(value + timezoneMinutesOffUtc * 60000).toISOString()
+		var hms = str.substring(11, 19)
+		var date = str.substring(0, 10)
+		if (timezoneMinutesOffUtc) {
+			var absoluteValue = Math.abs(timezoneMinutesOffUtc)
+			hms += (timezoneMinutesOffUtc < 0 ? '-' : '+') + getTwoDigits(absoluteValue / 60)
+			var minutes = absoluteValue % 60
+			if (minutes) hms += getTwoDigits(minutes)
+		} else {
+			hms += 'Z'
+		}
+		var result = hms + ' on ' + date
+	}
+	return result
+}
+function getTwoDigits(number) {
+	var result = number.toString()
+	if (result.length < 2) {
+			result = '0' + result
+	}
+	return result
+}
+function isDs(dateUtc) {
+
+	// calculate in the utc time zone
+	// 2am est = 6am utc
+
+	// Daylight time savings starts at 2 am est
+	// The second Sunday in March
+	// March 1, 2 am
+	var year = dateUtc.getUTCFullYear()
+	var march = 2
+	var november = 10
+	var dateDstBegin = new Date(Date.UTC(year, march, 1, 6))
+	var weekday = dateDstBegin.getUTCDay()
+	// number of days to get to second Sunday
+	var days = weekday == 0 ? 7 : 14 - weekday
+	dateDstBegin = new Date(Date.UTC(year, march, 1 + days, 6))
+
+	// Daylight time savings ends at 2 am first Sunday in November est
+	var dateDstEnd = new Date(Date.UTC(year, november, 1, 6))
+	weekday = dateDstEnd.getUTCDay()
+	if (days != 0) {
+		dateDstEnd = new Date(Date.UTC(year, november, 1 + 7 - weekday, 6))
+	}
+
+	return dateUtc >= dateDstBegin && dateUtc < dateDstEnd
 }
