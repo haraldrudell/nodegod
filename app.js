c2e68a91392155a90449d05892c700b340f8f8fd
Harald Rudell <harald@therudells.com>
2013-01-09 23:46:05 -0800

    ui launch with array, custom log filename, max 10 bad ui launches, more pidlink

--- app.js
+++ app.js
@@ -5,11 +5,14 @@
 var rotatedlogger = require('./lib/master/rotatedlogger')
 var processfinder = require('./lib/master/processfinder')
 var uimanager = require('./lib/master/uimanager')
+// http://nodejs.org/api/path.html
+var path = require('path')
 
 var appIndentifier = 'nodegodmaster'
 var port = 1113
 var interface = '127.0.0.1'
 var ignoredSignals = ['SIGINT', 'SIGUSR2']
+var launchArray = ['node'/*, '--debug-brk'*/, path.join(__dirname, 'webprocess')]
 
 var processName = appIndentifier + ':' + process.pid
 var log = rotatedlogger.log
@@ -24,10 +27,10 @@ ignoredSignals.forEach(function (signal) {
 processfinder.isProcessMaster({port: port, interface: interface, processName: processName, log: log}, masterResult)
 function masterResult(isMaster) {
 	if (isMaster === true) { // we need to launch the Web ui
-		rotatedlogger.init({logToFile: true}) // start writing to the logFile
+		rotatedlogger.init({logToFile: true, logFile: appIndentifier}) // start writing to the logFile
 		log(processName, 'is master: launching ui', launchTime)
 		processfinder.setResetUi(uimanager.getUiRelauncher())
-		uimanager.launchUi({processName: processName, uiModuleName: 'webprocess', log: log})
+		uimanager.launchUi({processName: processName, launchArray: launchArray, log: log})
 	} else if (typeof isMaster == 'number' && isMaster) { // there is already another nodegod master running, it will launch the web ui
 		log(processName, 'exiting: notified existing master with process id:', isMaster)
 	} else log(processName, 'failure communicating with exiting master:', isMaster instanceof Error ? isMaster.message : isMaster)
